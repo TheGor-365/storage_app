cb828e9bbb3ad6f827fc584b5fdf709501ef6057
Gor <7495500@gmail.com>
2021-02-16 17:15:57 +0300

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'pins#index'
   resources :pins
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
